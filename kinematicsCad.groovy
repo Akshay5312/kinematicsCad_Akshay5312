09afbf4a0a299d51ad930a1ceb4e076ed1597347
madhephaestus <mad.hephaestus@gmail.com>
2016-01-02 11:16:54 -0500

    removing scripting widget/n/n

--- kinematicsCad.groovy
+++ kinematicsCad.groovy
@@ -2,7 +2,7 @@ import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
 
 //Create the kinematics model from the xml file describing the D-H compliant parameters. 
-String xmlContent = ScriptingEngineWidget.codeFromGistID("2b0cff20ccee085c9c36","TrobotLinks.xml")[0];
+String xmlContent = ScriptingEngine.codeFromGistID("2b0cff20ccee085c9c36","TrobotLinks.xml")[0];
 MobileBase base = new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"));
 DHParameterKinematics model = base.getAppendages().get(0); 
 // Use Creature Lab to set the CAD script and customize your model. 
