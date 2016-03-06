1b26a8595abbf5be723bfce9291b7f0d20ee0ab1
madhephaestus <mad.hephaestus@gmail.com>
2016-03-06 12:08:32 -0500

    adding a walking cycle to the kinematics control demo/n/n

--- kinematicsCad.groovy
+++ kinematicsCad.groovy
@@ -2,20 +2,35 @@ import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
 
 MobileBase base;
-Object dev = DeviceManager.getSpecificDevice(MobileBase.class, "TrobotArmGroup");
+Object dev = DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot");
 println "found: "+dev
 //Check if the device already exists in the device Manager
 if(dev==null){
 	//Create the kinematics model from the xml file describing the D-H compliant parameters. 
-	String xmlContent = ScriptingEngine.codeFromGistID("2b0cff20ccee085c9c36","TrobotLinks.xml")[0];
-	return new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"));
+	def file=["https://gist.github.com/bcb4760a449190206170.git","CarlTheRobot.xml"]as String[]
+	String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
+	MobileBase mb =new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"))
+	mb.setGitSelfSource(file)
+	mb.connect()
+	DeviceManager.addConnection(mb,mb.getScriptingName())
+	base=mb
+	ThreadUtil.wait(10000)// wait for the cad to generate itself
 }else{
-	println "Arm found, runing code"
+	println "Robot found, runing code"
 	//the device is already present on the system, load the one that exists.
   	base=(MobileBase)dev
 }
 
-DHParameterKinematics model = base.getAppendages().get(0); 
-// Use Creature Lab to set the CAD script and customize your model. 
+// walk forward 10 increments 
+TransformNR move = new TransformNR(10,0,0,new RotationNR())
+double toSeconds=0.1//100 ms for each increment
+for(int i=0;i<10;i++){
+	base.DriveArc(move, toSeconds);
+	ThreadUtil.wait((int)toSeconds*1000)
+}
+
+ThreadUtil.wait(6000)// wait for the legs to fully reset themselves. 
+
+
 
 return null;
