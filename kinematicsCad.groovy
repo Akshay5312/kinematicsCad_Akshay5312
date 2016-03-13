61a535d78658e7e5c6b279198da0a4e8b18ea201
madhephaestus <mad.hephaestus@gmail.com>
2016-03-13 15:14:18 -0400

    more universal motions/n/n

--- kinematicsCad.groovy
+++ kinematicsCad.groovy
@@ -5,22 +5,25 @@ MobileBase base;
 Object dev = DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot");
 println "found: "+dev
 //Check if the device already exists in the device Manager
-if(dev==null){
-	//Create the kinematics model from the xml file describing the D-H compliant parameters. 
-	def file=["https://gist.github.com/bcb4760a449190206170.git","CarlTheRobot.xml"]as String[]
-	String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
-	MobileBase mb =new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"))
-	mb.setGitSelfSource(file)
-	mb.connect()
-	DeviceManager.addConnection(mb,mb.getScriptingName())
-	base=mb
-	println "Waiting for cad to generate"
-	ThreadUtil.wait(50000)// wait for the cad to generate itself
-}else{
-	println "Robot found, runing code"
-	//the device is already present on the system, load the one that exists.
-  	base=(MobileBase)dev
-}
+if(args==null){
+	if(dev==null){
+		//Create the kinematics model from the xml file describing the D-H compliant parameters. 
+		def file=["https://gist.github.com/bcb4760a449190206170.git","CarlTheRobot.xml"]as String[]
+		String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
+		MobileBase mb =new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"))
+		mb.setGitSelfSource(file)
+		mb.connect()
+		DeviceManager.addConnection(mb,mb.getScriptingName())
+		base=mb
+		println "Waiting for cad to generate"
+		ThreadUtil.wait(50000)// wait for the cad to generate itself
+	}else{
+		println "Robot found, runing code"
+		//the device is already present on the system, load the one that exists.
+	  	base=(MobileBase)dev
+	}
+}else
+	base=(MobileBase)args.get(0)
 
 // walk forward 10 increments of 10 mm totalling 100 mm translation
 TransformNR move = new TransformNR(10,0,0,new RotationNR())
@@ -47,13 +50,13 @@ ThreadUtil.wait(6000)// wait for the legs to fully reset themselves.
 println "Now to move one limb" 
 // Now we will move just one leg
 DHParameterKinematics leg0 = base.getAllDHChains().get(3)
-double zLift=15
+double zLift=5
 //Start from where the arm already is and move it from there with absolute location
 TransformNR current = leg0.getCurrentPoseTarget();
 current.translateZ(zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
 ThreadUtil.wait(2000)// wait for the legs to fully arrive
-println "Resetting limb"
+
 //and reset it
 current.translateZ(-zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
@@ -62,7 +65,6 @@ ThreadUtil.wait(2000)// wait for the legs to fully arrive
 current.translateX(zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
 ThreadUtil.wait(2000)// wait for the legs to fully arrive
-println "Resetting limb"
 //and reset it
 current.translateX(-zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
@@ -71,7 +73,6 @@ ThreadUtil.wait(2000)// wait for the legs to fully arrive
 current.translateY(-zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
 ThreadUtil.wait(2000)// wait for the legs to fully arrive
-println "Resetting limb"
 //and reset it
 current.translateY(zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
