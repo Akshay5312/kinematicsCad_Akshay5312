26cea352619d1124a87653e62e64e2be6f58978c
BFTwChees <64285036+BFTwChees@users.noreply.github.com>
2021-04-09 11:55:26 -0400

    I accessed and printed DH parameters from the heaphustus arm
    I wanted to write the DH parameters onto a JSON file

--- kinematicsCad.groovy
+++ kinematicsCad.groovy
@@ -1,10 +1,15 @@
+import com.neuronrobotics.bowlerstudio.creature.MobileBaseLoader
 import com.neuronrobotics.bowlerstudio.threed.*;
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.common.DeviceManager
+
 import org.apache.commons.io.*
 
-def base;
+MobileBase base;
 //Check if the device already exists in the device Manager
 if(args==null){
-	base=DeviceManager.getSpecificDevice( "HephaestusArmV2_copy",{
+	base=(MobileBase)DeviceManager.getSpecificDevice( "HephaestusArmV2_copy",{
 			//If the device does not exist, prompt for the connection
 			
 			MobileBase m = MobileBaseLoader.fromGit(
@@ -18,3 +23,19 @@ if(args==null){
 		})
 }else
 	base=args.get(0)
+	
+def Limbs = base.getAllDHChains()
+	
+for(DHParameterKinematics limb: Limbs) {
+	int NoLimbs = limb.numberOfLinks
+	def name = limb.getScriptingName()
+	def limbRoot = limb.getRobotToFiducialTransform()
+	println "name = "+ name + " limbRoot = " + limbRoot 
+	for(int i = 0; i <NoLimbs; i++) {
+		def Alpha = limb.getDH_Alpha(i)
+		def D = limb.getDH_D(i)
+		def R = limb.getDH_R(i)
+		def Theta = limb.getDH_Theta(i)
+		println "DH Parameters; Alpha = "+Math.toDegrees(Alpha)+" D = "+D+" R = "+R+" Theta = "+Math.toDegrees(Theta)
+	}
+}
