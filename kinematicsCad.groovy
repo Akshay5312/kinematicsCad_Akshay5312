0be6e22208388c0a868f11f59f76720ab224ec4f
BFTwChees <64285036+BFTwChees@users.noreply.github.com>
2021-04-09 11:12:33 -0400

    Ignore the project files

--- kinematicsCad.groovy
+++ kinematicsCad.groovy
@@ -4,12 +4,12 @@ import org.apache.commons.io.*
 def base;
 //Check if the device already exists in the device Manager
 if(args==null){
-	base=DeviceManager.getSpecificDevice( "CarlTheWalkingRobot",{
+	base=DeviceManager.getSpecificDevice( "HephaestusArmV2_copy",{
 			//If the device does not exist, prompt for the connection
 			
 			MobileBase m = MobileBaseLoader.fromGit(
-				"https://github.com/madhephaestus/carl-the-hexapod.git",
-				"CarlTheRobot.xml"
+				"https://github.com/Akshay5312/HephaestusArmV2_copy.git",
+				"HephaestusArmV2_copy.xml"
 				)
 			if(m==null)
 				throw new RuntimeException("Arm failed to assemble itself")
@@ -18,71 +18,3 @@ if(args==null){
 		})
 }else
 	base=args.get(0)
-
-
-// walk forward 10 increments of 10 mm totalling 100 mm translation
-TransformNR move = new TransformNR(10,0,0,new RotationNR())
-double toSeconds=0.1//100 ms for each increment
-for(int i=0;i<10;i++){
-	base.DriveArc(move, toSeconds);
-	ThreadUtil.wait((int)toSeconds*1000)
-}
-// turn 20 increments of 2 degrees totalling 40 degrees turn
-move = new TransformNR(0,0,0,new RotationNR( 0,0, 2))
-for(int i=0;i<20;i++){
-	base.DriveArc(move, toSeconds);
-	ThreadUtil.wait((int)toSeconds*1000)
-}
-// walk sideways 10 increments of 10 mm totalling 100 mm translation
-move = new TransformNR(0,10,0,new RotationNR())
-for(int i=0;i<10;i++){
-	base.DriveArc(move, toSeconds);
-	ThreadUtil.wait((int)toSeconds*1000)
-}
-
-println "Waiting for legs to reset"
-ThreadUtil.wait(12000)// wait for the legs to fully reset themselves.
-println "Now to move one limb" 
-// Now we will move just one leg
-DHParameterKinematics leg0 = base.getAllDHChains().get(0)
-double zLift=5
-//Start from where the arm already is and move it from there with absolute location
-TransformNR current = leg0.getCurrentPoseTarget();
-current.translateZ(zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-
-//and reset it
-current.translateZ(-zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-
-current.translateX(zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-//and reset it
-current.translateX(-zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-
-current.translateY(-zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-//and reset it
-current.translateY(zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
-
-println "Now move just one link"
-for(int i=0;i<leg0.getNumberOfLinks();i++){
-	leg0.setDesiredJointAxisValue(i,// link index
-							25, //target angle
-							2.0) // 2 seconds
-	ThreadUtil.wait(2000)// wait for the link to fully arrive
-	
-	leg0.setDesiredJointAxisValue(i,// link index
-							0, //target angle
-							2.0) // 2 seconds
-	ThreadUtil.wait(2000)// wait for the link to fully arrive
-}
-return null;
